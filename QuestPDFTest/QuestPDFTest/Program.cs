1db325d9fa8774c5ae15d3b281053ba39b60e1f5
羅肖 <xiao.luo@sbibits.com>
2024-07-31 17:50:31 +0900

    modify
    update

--- QuestPDFTest/QuestPDFTest/Program.cs
+++ QuestPDFTest/QuestPDFTest/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
+//官网 https://www.questpdf.com/
 
 // 1、请确保您有资格使用社区许可证，不设置的话会报异常。
 using QuestPDF.Infrastructure;
@@ -15,11 +15,4 @@ Settings.CheckIfAllTextGlyphsAreAvailable = false;
 
 var reportCreator = new ReportDocument();
 reportCreator.GeneratePdfAndShow();
-
-// 3、PDF Document 创建
-//var invoiceSourceData = CreateInvoiceDetails.GetInvoiceDetails();
-//var document = new CreateInvoiceDocument(invoiceSourceData);
-
-//// 4、生成 PDF 文件并在默认的查看器中显示
-//document.GeneratePdfAndShow();
 
